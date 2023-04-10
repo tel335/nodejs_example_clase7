f67cd9264ae9284a02564519785a2f12fc80621a
Francisco Cabezas <francisco.cabezas@xkivertech.cl>
2023-04-10 19:00:29 -0400

    (FEATURE): Se agrega un servicio health del tipo GET

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hola Mundo!')
+import koa from 'koa'
+import bodyParser from 'koa-body'
+import router from './router/index'
+
+const app = new koa()
+const port = 3000
+
+app.use(bodyParser({ multipart: true, urlencoded: true }))
+app.use(router.routes())
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
